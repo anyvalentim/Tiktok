dafd70f02275548985b8b0accaa1245868d3bcb9
anyvalentim <145420174+anyvalentim@users.noreply.github.com>
2023-09-19 01:33:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Substitua com a sua lógica de integração com a API do TikTok para capturar presentes
+function receberPresente() {
+    // Lógica para reagir ao presente recebido
+    const imagem = document.getElementById('imagem');
+    imagem.src = 'imagem_reagindo.gif'; // Substitua com o caminho da imagem reativa
+}
+
+// Exemplo de chamada da função quando um presente é recebido
+// Suponha que você tenha um evento ou uma função que é chamada quando um presente é recebido na live
+// Chame receberPresente() quando um presente for recebido
